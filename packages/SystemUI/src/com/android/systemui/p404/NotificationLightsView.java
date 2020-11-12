AOSPA/android_frameworks_base-old

SystemUI: add Pulse and Ambient notification bars[1/2]
[maxwen] SystemUI: support using notification color for pulse light

optional use the color information of the notification
as color for notification pulse light

@brituj
* Update color when a new notification arrives while pulsing
* Fix row == null when on lockscreen even though the notification is clearable
* other code improvements

@mickaelmendes50
* Adapt to Android 11

-------------------------------------------------------------------
[SQUASH]
Add Pulse and Ambient Display bars for notifications

Change-Id: Ic5fdbdf05ef3f836e0c42ecef30e791a1f7eeb6e

SystemUI: trigger pulse light only for notifications

store the doze trigger reason in a setting to check later
if it has been triggered for a notification or any other reason

Change-Id: Iecee97b04e5e943c766fb4794d89e036ded65b3a

SystemUI: Pulse light accent color option

Change-Id: If402e96d7549aff7f758236e6f54a6034d08f9c2

SystemUI: use ROWS_HIGH_PRIORITY for active notification pulse check

only one of those could make pulse happen anyway

Change-Id: I002344f321bdfed770f1099d7d33edc35671d997

SystemUI: replace pulse light drawable with our own vector
--------------------------------------------------------------------

Co-authored-by: maxwen <max.weninger@gmail.com>
Signed-off-by: rituj <ritujbeniwal@gmail.com>
Change-Id: I95756d9886b2f4120d5ece72418850b100fe7394

packages/SystemUI/src/com/android/systemui/aospa/NotificationLightsView.java
@@ -1,0 +1,117 @@
+/*
+* Copyright (C) 2019 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.android.systemui.aospa;
+
+import android.animation.ValueAnimator;
+import android.animation.ValueAnimator.AnimatorUpdateListener;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.android.settingslib.Utils;
+import com.android.systemui.R;
+
+public class NotificationLightsView extends RelativeLayout {
+    private static final boolean DEBUG = false;
+    private static final String TAG = "NotificationLightsView";
+
+    private ValueAnimator mLightAnimator;
+    private ImageView mLeftView;
+    private ImageView mRightView;
+    private AnimatorUpdateListener mAnimatorUpdateListener = new AnimatorUpdateListener() {
+        public void onAnimationUpdate(ValueAnimator animation) {
+            if (DEBUG) Log.d(TAG, "onAnimationUpdate");
+            float progress = ((Float) animation.getAnimatedValue()).floatValue();
+            mLeftView.setScaleY(progress);
+            mRightView.setScaleY(progress);
+            float alpha = 1.0f;
+            if (progress <= 0.3f) {
+                alpha = progress / 0.3f;
+            } else if (progress >= 1.0f) {
+                alpha = 2.0f - progress;
+            }
+            mLeftView.setAlpha(alpha);
+            mRightView.setAlpha(alpha);
+        }
+    };
+
+    public NotificationLightsView(Context context) {
+        this(context, null);
+    }
+
+    public NotificationLightsView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public NotificationLightsView(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public NotificationLightsView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        if (DEBUG) Log.d(TAG, "new");
+        mLightAnimator = ValueAnimator.ofFloat(new float[]{0.0f, 2.0f});
+        mLightAnimator.setDuration(2000);
+        mLightAnimator.setRepeatMode(ValueAnimator.RESTART);
+    }
+
+    public void animateNotification() {
+        animateNotificationWithColor(getNotificationLightsColor());
+    }
+
+    public int getNotificationLightsColor() {
+        int color = getDefaultNotificationLightsColor();
+        boolean useAccent = Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.AMBIENT_NOTIFICATION_LIGHT_AUTOMATIC, 1, UserHandle.USER_CURRENT) == 0;
+        if (useAccent) {
+            color = Utils.getColorAccentDefaultColor(getContext());
+        }
+        return color;
+    }
+
+    public int getDefaultNotificationLightsColor() {
+        return getResources().getInteger(com.android.internal.R.integer.config_AmbientPulseLightColor);
+    }
+
+    public void endAnimation() {
+        mLightAnimator.end();
+        mLightAnimator.removeAllUpdateListeners();
+    }
+
+    public void animateNotificationWithColor(int color) {
+        if (mLeftView == null) {
+            mLeftView = (ImageView) findViewById(R.id.notification_animation_left);
+        }
+        if (mRightView == null) {
+            mRightView = (ImageView) findViewById(R.id.notification_animation_right);
+        }
+        mLeftView.setColorFilter(color);
+        mRightView.setColorFilter(color);
+        if (!mLightAnimator.isRunning()) {
+            if (DEBUG) Log.d(TAG, "start");
+            mLightAnimator.setRepeatCount(ValueAnimator.INFINITE);
+            mLightAnimator.addUpdateListener(mAnimatorUpdateListener);
+            mLightAnimator.start();
+        }
+    }
+}

packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
@@ -802,8 +802,27 @@ public class NotificationStackScrollLayout extends ViewGroup implements ScrollAd
         return false;
     }
 
-  @ShadeViewRefactor(RefactorComponent.SHADE_VIEW)
-  public RemoteInputController.Delegate createDelegate() {
+    /** @hide */
+    public ExpandableNotificationRow getFirstActiveClearableNotification(@SelectedRows int selection) {
+        if (mDynamicPrivacyController.isInLockedDownShade()) {
+            return null;
+        }
+        int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+            if (!(child instanceof ExpandableNotificationRow)) {
+                continue;
+            }
+            final ExpandableNotificationRow row = (ExpandableNotificationRow) child;
+            if (row.getStatusBarNotification().isClearable() && matchesSelection(row, selection)) {
+                return row;
+            }
+        }
+        return null;
+    }
+
+    @ShadeViewRefactor(RefactorComponent.SHADE_VIEW)
+    public RemoteInputController.Delegate createDelegate() {
         return new RemoteInputController.Delegate() {
             public void setRemoteInputActive(NotificationEntry entry,
                     boolean remoteInputActive) {
@@ -3324,6 +3343,9 @@ public class NotificationStackScrollLayout extends ViewGroup implements ScrollAd
     public void onViewAdded(View child) {
         super.onViewAdded(child);
         onViewAddedInternal((ExpandableView) child);
+        if (child instanceof ExpandableNotificationRow) {
+            mNotificationPanel.updateAmbientPulse();
+        }
     }
 
     @ShadeViewRefactor(RefactorComponent.STATE_RESOLVER)

404-Mess/frameworks_base

packages/SystemUI/src/com/android/systemui/p404/NotificationLightsView.java
@@ -1,0 +1,117 @@
+/*
+* Copyright (C) 2019 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.android.systemui.p404;
+
+import android.animation.ValueAnimator;
+import android.animation.ValueAnimator.AnimatorUpdateListener;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.android.settingslib.Utils;
+import com.android.systemui.R;
+
+public class NotificationLightsView extends RelativeLayout {
+    private static final boolean DEBUG = false;
+    private static final String TAG = "NotificationLightsView";
+
+    private ValueAnimator mLightAnimator;
+    private ImageView mLeftView;
+    private ImageView mRightView;
+    private AnimatorUpdateListener mAnimatorUpdateListener = new AnimatorUpdateListener() {
+        public void onAnimationUpdate(ValueAnimator animation) {
+            if (DEBUG) Log.d(TAG, "onAnimationUpdate");
+            float progress = ((Float) animation.getAnimatedValue()).floatValue();
+            mLeftView.setScaleY(progress);
+            mRightView.setScaleY(progress);
+            float alpha = 1.0f;
+            if (progress <= 0.3f) {
+                alpha = progress / 0.3f;
+            } else if (progress >= 1.0f) {
+                alpha = 2.0f - progress;
+            }
+            mLeftView.setAlpha(alpha);
+            mRightView.setAlpha(alpha);
+        }
+    };
+
+    public NotificationLightsView(Context context) {
+        this(context, null);
+    }
+
+    public NotificationLightsView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public NotificationLightsView(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public NotificationLightsView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        if (DEBUG) Log.d(TAG, "new");
+        mLightAnimator = ValueAnimator.ofFloat(new float[]{0.0f, 2.0f});
+        mLightAnimator.setDuration(2000);
+        mLightAnimator.setRepeatMode(ValueAnimator.RESTART);
+    }
+
+    public void animateNotification() {
+        animateNotificationWithColor(getNotificationLightsColor());
+    }
+
+    public int getNotificationLightsColor() {
+        int color = getDefaultNotificationLightsColor();
+        boolean useAccent = Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.AMBIENT_NOTIFICATION_LIGHT_AUTOMATIC, 1, UserHandle.USER_CURRENT) == 0;
+        if (useAccent) {
+            color = Utils.getColorAccentDefaultColor(getContext());
+        }
+        return color;
+    }
+
+    public int getDefaultNotificationLightsColor() {
+        return getResources().getInteger(com.android.internal.R.integer.config_AmbientPulseLightColor);
+    }
+
+    public void endAnimation() {
+        mLightAnimator.end();
+        mLightAnimator.removeAllUpdateListeners();
+    }
+
+    public void animateNotificationWithColor(int color) {
+        if (mLeftView == null) {
+            mLeftView = (ImageView) findViewById(R.id.notification_animation_left);
+        }
+        if (mRightView == null) {
+            mRightView = (ImageView) findViewById(R.id.notification_animation_right);
+        }
+        mLeftView.setColorFilter(color);
+        mRightView.setColorFilter(color);
+        if (!mLightAnimator.isRunning()) {
+            if (DEBUG) Log.d(TAG, "start");
+            mLightAnimator.setRepeatCount(ValueAnimator.INFINITE);
+            mLightAnimator.addUpdateListener(mAnimatorUpdateListener);
+            mLightAnimator.start();
+        }
+    }
+}

packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
@@ -802,8 +802,27 @@ public class NotificationStackScrollLayout extends ViewGroup implements ScrollAd
         return false;
     }
 
-  @ShadeViewRefactor(RefactorComponent.SHADE_VIEW)
-  public RemoteInputController.Delegate createDelegate() {
+    /** @hide */
+    public ExpandableNotificationRow getFirstActiveClearableNotification(@SelectedRows int selection) {
+        if (mDynamicPrivacyController.isInLockedDownShade()) {
+            return null;
+        }
+        int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+            if (!(child instanceof ExpandableNotificationRow)) {
+                continue;
+            }
+            final ExpandableNotificationRow row = (ExpandableNotificationRow) child;
+            if (row.getEntry().getSbn().isClearable() && matchesSelection(row, selection)) {
+                return row;
+            }
+        }
+        return null;
+    }
+
+    @ShadeViewRefactor(RefactorComponent.SHADE_VIEW)
+    public RemoteInputController.Delegate createDelegate() {
         return new RemoteInputController.Delegate() {
             public void setRemoteInputActive(NotificationEntry entry,
                     boolean remoteInputActive) {
@@ -3324,6 +3343,9 @@ public class NotificationStackScrollLayout extends ViewGroup implements ScrollAd
     public void onViewAdded(View child) {
         super.onViewAdded(child);
         onViewAddedInternal((ExpandableView) child);
+        if (child instanceof ExpandableNotificationRow) {
+            mNotificationPanelController.updateAmbientPulse();
+        }
     }
 
     @ShadeViewRefactor(RefactorComponent.STATE_RESOLVER)